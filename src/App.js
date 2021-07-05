4698a00100343a9863aafe7062e043d0c693b74d
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2021-07-05 15:15:09 +0900

    react basic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-const App = () =>{
+import React, { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const increment2 = () => setCount(prviousCount => prviousCount + 1)
+  const decrement2 = () => setCount(prviousCount => prviousCount - 1)
+  const reset = () => setCount(0)
+  const double = () => setCount(prviousCount => prviousCount * 2)
+  const three = () => {
+    if(count % 3 === 0){
+      setCount(prviousCount => prviousCount / 3)
+    }
+  }
+
   return (
-    <div>
-      hello world
-    </div>
+    <React.Fragment>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment} >+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2} >+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset} >reset</button>
+        <button onClick={double} >double</button>
+        <button onClick={three} >3 special</button>
+      </div>
+    </React.Fragment>
   )
 }
 
