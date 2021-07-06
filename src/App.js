0d58b60a0d78f1d335ff5006465aaa2d5ed38a56
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2021-07-06 10:26:54 +0900

    implemnt component usuing multipleState

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0)
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const increment2 = () => setCount(prviousCount => prviousCount + 1)
-  const decrement2 = () => setCount(prviousCount => prviousCount - 1)
-  const reset = () => setCount(0)
-  const double = () => setCount(prviousCount => prviousCount * 2)
-  const three = () => {
-    if(count % 3 === 0){
-      setCount(prviousCount => prviousCount / 3)
-    }
-  }
+const App = props => {
+  const [name,setName] = useState(props.name)
+  const [price,setPrice] = useState(props.price)
 
+  const reset = () =>{
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
     <React.Fragment>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment} >+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2} >+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset} >reset</button>
-        <button onClick={double} >double</button>
-        <button onClick={three} >3 special</button>
-      </div>
+     <p>{name}'s price is ${price}</p>
+     <button onClick = {() => setPrice(price + 1)}>+1</button>
+     <button onClick = {() => setPrice(price - 1)}>-1</button>
+     <button onClick = {reset}>Reset</button>
+     <input value = {name} onChange = {e => setName(e.target.value)}/>
     </React.Fragment>
   )
+}
+
+App.defaultProps = {
+    name: '',
+    price: 1000
 }
 
 export default App
