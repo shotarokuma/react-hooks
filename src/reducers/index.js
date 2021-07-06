e77f9d5c9ed480bca4759e956ec2f929fa4b9c8c
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2021-07-06 14:57:59 +0900

    create reducers for event

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const events = (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_EVENT':
+      const event = { title: action.title, body: action.body }
+      const length = state.length
+      let id = length === 0 ? id = 1 : id = state[lenght - 1] + 1
+      return [...state, {id,...event}]
+    case 'DELETE_EVENT':
+    case 'DELETE_ALL_EVENT':
+      return []
+    default:
+      return state
+  }
+}
+
+export default events
